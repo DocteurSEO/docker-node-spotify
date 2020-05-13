45df8fc10293419f53e33ab490a5e0b744a9f4e4
docteurseo <docteurseoo@gmail.com>
2020-05-13 07:43:33 +0200

    merge

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react';
 import './App.css';
 
@@ -8,13 +7,6 @@ function App() {
     ... 
     </div>
   );
-=======
-import React from "react";
-import "./App.css";
-import Header from "./components/header";
-function App() {
-  return <div className="App"></div>;
->>>>>>> dev
 }
 
 export default App;
